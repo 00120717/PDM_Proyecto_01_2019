f79d70dbf5f2d6687e19204b9584084d1eca6d6a
00127417 <00127417>
2019-06-20 15:36:50 -0600

    cambios

--- app/src/main/java/com/coclearapp/pdm_project/Fragment/GridExercisesFragment.kt
+++ app/src/main/java/com/coclearapp/pdm_project/Fragment/GridExercisesFragment.kt
@@ -7,13 +7,23 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.coclearapp.pdm_project.Activities.LevelsActivity
+import com.coclearapp.pdm_project.Adapters.PatientAdapter
+import com.coclearapp.pdm_project.Models.Patient
 
 import com.coclearapp.pdm_project.R
 
 import com.coclearapp.pdm_project.ViewModel.LevelViewModel
+import kotlinx.android.synthetic.main.activity_patients.*
+import kotlinx.android.synthetic.main.fragment_grid_exercises.*
 
 class GridExercisesFragment(level: Int): Fragment(){
 
+
+    private lateinit var viewAdapter: PatientAdapter
+    private lateinit var viewManager: RecyclerView.LayoutManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,15 +32,40 @@ class GridExercisesFragment(level: Int): Fragment(){
         } ?: throw Exception("Invalid Activity")
 
     }
+
+
+    fun initRecycle(patients : List<Patient>){
+
+    }
+
+
+
+    private fun patientItemClicked(item: Patient){
+
+        startActivity(Intent(this.context, LevelsActivity::class.java).putExtra("name",item.name))
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_grid_exercises, container, false)
+
+        var view = inflater.inflate(R.layout.fragment_grid_exercises, container, false)
+
+        rview.apply {
+            layoutManager = LinearLayoutManager(this.context)
+
+        }
+
+
+
+        return view
 
 
         }
+
+
 
     companion object {
         @JvmStatic
