154636ad81c953bcdbacf7be71d53672be74fab2
00127417 <00127417>
2019-06-24 10:42:56 -0600

    cambios patient

--- app/src/main/java/com/coclearapp/pdm_project/Repository/PatientRepository.kt
+++ app/src/main/java/com/coclearapp/pdm_project/Repository/PatientRepository.kt
@@ -1,15 +1,17 @@
 package com.coclearapp.pdm_project.Repository
 
 import androidx.annotation.WorkerThread
+import androidx.lifecycle.LiveData
 import com.coclearapp.pdm_project.Room.Dao.PatientDao
 import com.coclearapp.pdm_project.Room.Entity.Patient
 
 class PatientRepository(private val patientDao: PatientDao){
 
+    fun getAllPatients():LiveData<List<Patient>> = patientDao.getAllPatient()
+
     @WorkerThread
     suspend  fun insertPatient(patient: Patient) {
         patientDao.insertPatient(patient)
     }
-
 
 }
--- app/src/main/java/com/coclearapp/pdm_project/ViewModel/PatientViewModel.kt
+++ app/src/main/java/com/coclearapp/pdm_project/ViewModel/PatientViewModel.kt
@@ -2,7 +2,29 @@ package com.coclearapp.pdm_project.ViewModel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.coclearapp.pdm_project.Repository.PatientRepository
+import com.coclearapp.pdm_project.Room.CoclearRoomDatabase
+import com.coclearapp.pdm_project.Room.CoclearRoomDatabase_Impl
+import com.coclearapp.pdm_project.Room.Entity.Patient
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class PatientViewModel(application: Application) : AndroidViewModel(application) {
 
+    private val patientRepo: PatientRepository
+
+    init {
+        val patientDao = CoclearRoomDatabase.getDatabase(application,viewModelScope).PatientDao()
+
+        patientRepo = PatientRepository(patientDao)
+    }
+
+    //funcion obtener pacientes
+    fun getAllPatients() = patientRepo.getAllPatients()
+
+    //funcion insertar paciente
+    fun insertPatient(patient: Patient) = viewModelScope.launch(Dispatchers.IO) {
+        patientRepo.insertPatient(patient)
+    }
 }
